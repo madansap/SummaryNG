2103768eb5679b3f5813b517cf7045faaaa1d81d
madansap <namastemadan.sapkota@gmail.com>
2025-02-18 19:20:52 +0545

    dignin error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        undici: false,
+      };
+    }
+    return config;
+  },
   experimental: {
-    serverActions: true,
+    // Remove serverActions flag as it's enabled by default now
+    serverComponentsExternalPackages: ['better-sqlite3']
   },
   env: {
     DB: process.env.DB,
